5c0a6c5dc94493cbd1f2f9a383be400017df6bc2
Arnaud Coulon <arnaud-coulon@outlook.fr>
2018-07-19 11:20:01 +0200

    Update inventory.py
    Added an exception management

--- inventory.py
+++ inventory.py
@@ -7,14 +7,16 @@ from datetime import datetime
 path = yourpath
 df = pd.DataFrame(columns=list("ABCD"))
 
-#loop walking throught the path. Get Name, last modification date, dir and size' file
+#Loop walking throught the path. Gets Name, last modification date, dir and size' file
+#Printing FileNotFoundError paths
+try :
+    for root,dirs,files in os.walk(path):
+        for file in files:
 
-for root,dirs,files in os.walk(path):
-    for file in files:
-
-        chemin = join(root,file)
-        df = df.append(pd.DataFrame({"A" :[file],"B" :[datetime.fromtimestamp(os.path.getctime(chemin)).strftime('%d/%m/%Y')], "C" : [chemin],"D" : [os.path.getsize(chemin)]}))
-
+            chemin = join(root,file)
+            df = df.append(pd.DataFrame({"A" :[file],"B" :[datetime.fromtimestamp(os.path.getctime(chemin)).strftime('%d/%m/%Y')], "C" : [chemin],"D" : [os.path.getsize(chemin)]}))
+except FileNotFoundError:
+    print(chemin)
 
 #Loading in an Excel file
 df.to_excel("C:/Users/984098/Desktop/rgpd.xlsx")
