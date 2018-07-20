c73a1695f13da2e86b807aa9fc915532d71f8a6c
Arnaud Coulon <arnaud-coulon@outlook.fr>
2018-07-20 10:21:20 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -8,15 +8,12 @@ path = yourpath
 df = pd.DataFrame(columns=list("ABCD"))
 
 #Loop walking throught the path. Gets Name, last modification date, dir and size' file
-#Printing FileNotFoundError paths
-try :
-    for root,dirs,files in os.walk(path):
-        for file in files:
 
-            chemin = join(root,file)
-            df = df.append(pd.DataFrame({"A" :[file],"B" :[datetime.fromtimestamp(os.path.getctime(chemin)).strftime('%d/%m/%Y')], "C" : [chemin],"D" : [os.path.getsize(chemin)]}))
-except FileNotFoundError:
-    print(chemin)
+for root,dirs,files in os.walk(path):
+    for file in files:
+
+        chemin = join(root,file)
+        df = df.append(pd.DataFrame({"A" :[file],"B" :[datetime.fromtimestamp(os.path.getctime(chemin)).strftime('%d/%m/%Y')], "C" : [chemin],"D" : [os.path.getsize(chemin)]}))
 
 #Loading in an Excel file
 df.to_excel("C:/Users/984098/Desktop/rgpd.xlsx")
