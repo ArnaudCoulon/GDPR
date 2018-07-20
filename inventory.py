294d706fd6bd8f556ace19d80cd36b794b442339
Arnaud Coulon <arnaud-coulon@outlook.fr>
2018-07-20 12:55:54 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -5,15 +5,16 @@ import pandas as pd
 from datetime import datetime
 
 path = yourpath
+root = yourroot
 df = pd.DataFrame(columns=list("ABCD"))
 
 #Loop walking throught the path. Gets Name, last modification date, dir and size' file
 
-for root,dirs,files in os.walk(path):
+for root,dirs,files in os.walk(yourroot):
     for file in files:
 
         chemin = join(root,file)
         df = df.append(pd.DataFrame({"A" :[file],"B" :[datetime.fromtimestamp(os.path.getctime(chemin)).strftime('%d/%m/%Y')], "C" : [chemin],"D" : [os.path.getsize(chemin)]}))
 
 #Loading in an Excel file
-df.to_excel("C:/Users/984098/Desktop/rgpd.xlsx")
+df.to_excel(path)
